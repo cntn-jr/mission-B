3ecbe244f5773cb36a07ed228cc593a4126de0c7
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2021-07-29 17:59:17 +0900

    change: index design
    インデックスページのデザインを変更しました
    
    ログアウトのリンクを追加しました

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const authMiddleware = (req, res, next) => {
+  if(req.isAuthenticated()) { // ログインしてるかチェック
+    next();
+  } else {
+    res.redirect(302, '/login');
+  }
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', authMiddleware, function(req, res, next) {
+  const data = {
+    user_name: req.user.name,
+  }
+  res.render('index', data);
 });
 
 module.exports = router;
